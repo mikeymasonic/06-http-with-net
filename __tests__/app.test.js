fb2d4f037f00dc2d8d13016fe07e6b6b11c30ab7
Michael Romay <amerror@gmail.com>
2020-03-16 15:33:05 -0700

    first app js test done

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,5 +2,12 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('createResponse', () => {
+  it('returns hi', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
+  });
 
 });
